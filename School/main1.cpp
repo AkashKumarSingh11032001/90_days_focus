ea2734bab579fbf2275f71c13be60690f4f24133
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-10-26 07:05:06 +0530

    find the elements whose value is equal to that of its index value

--- School/main1.cpp
+++ School/main1.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 // Notes:
 // 1-> 1 + 2 + 3 +... + n = n(n + 1) / 2
+// 2-> Your task is to find the elements whose value is equal to that of its index value ( Consider 1-based indexing ).
+
+void print1d(vector<int>arr){
+    for(auto i : arr) cout << i << " ";
+}
 
 long long seriesSum(int n)
 {
@@ -12,7 +17,21 @@ long long seriesSum(int n)
     return ((long)n * (long)(n + 1) / 2);
 }
 
+vector<int> valueEqualToIndex(int arr[], int n)
+{
+    // code here
+    vector<int> res;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == i + 1)
+            res.push_back(i + 1);
+    }
+    return res;
+}
+
 int main()
 {
     cout << seriesSum(5) << endl;
+    int arr[] = {15, 2, 34, 56, 76};
+    cout << print1d(valueEqualToIndex(arr, 5));
 }
